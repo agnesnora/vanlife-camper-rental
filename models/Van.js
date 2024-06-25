a40dc246f65a967ea82c98a272642f62c9e09912
agnesnora <agnesnora@gmail.com>
2024-06-25 11:28:53 +0200

    VanSchema

---
+++ models/Van.js
@@ -0,0 +1,93 @@
+import { Schema, model, models } from "mongoose";
+
+const VanSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    seat: {
+      type: Number,
+      required: true,
+    },
+    stars: {
+      type: Number,
+      required: true,
+    },
+
+    amenities: [
+      {
+        type: String,
+      },
+    ],
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+    seller_info: {
+      name: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Van = models.Van || model.Van("Van", VanSchema);
+
+export default Van;
